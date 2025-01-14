2be8e65c957baa586335341de0f31cc3fed84c46
Luiz_Guilherme <luiz.2022310970@aluno.iffar.edu.br>
2025-01-14 18:11:46 -0300

    .

--- Crud/crud_projeto/listar.php
+++ Crud/crud_projeto/listar.php
@@ -4,7 +4,7 @@ require_once("../../conecta.php");
 $conexao = conectar();
 
 // Seleciona todos os dados da tabela historia
-$sql = "SELECT * FROM projeto WHERE fk_projeto_id_professor =" . $_SESSION['usuario'][1];
+$sql = "SELECT * FROM projeto WHERE fk_projeto_id_professor =" .  $_SESSION['usuario'][1]." ORDER BY situacao ASC";
 
 // Executa o Select
 $resultado = mysqli_query($conexao, $sql);
@@ -16,27 +16,45 @@ if ($quantidade_linha == 0) {
     echo "Você não tem projetos cadastrados!";
 } else {
 
-    //Lista os itens
-    echo '<table border=1>
-<tr>
-</tr>';
+?>
 
-    while ($dados = mysqli_fetch_assoc($resultado)) {
-        if ($dados['situacao'] == "disponivel") {
-            echo '<tr>';
-            echo '<td>' . $dados['nome_projeto'] . '</td>';
-            echo '<td>' . $dados['situacao'] . '</td>';
-            echo '<td> <a href="../../Crud/crud_encontro/listar.php?id_projeto=' . $dados['id_projeto'] . '">encontros</a> </td>';
-            echo '<td> <a href="../../Crud/crud_projeto/finalizar.php?id_projeto=' . $dados['id_projeto'] . '">finalizar</a></td>';
-            echo '<td> <a href="../../Crud/crud_projeto/excluir.php?id_projeto=' . $dados['id_projeto'] . '">Excluir Projeto</a></td>';
-            echo '</tr>';
-        }else{
-            echo '<tr>';
-            echo '<td>' . $dados['nome_projeto'] . '</td>';
-            echo '<td>' . $dados['situacao'] . '</td>';
-            echo '<td> <a href="../../certificado.php?id_projeto=' . $dados['id_projeto'] . '"> Certificado </a></td>';
-            echo '</tr>';
-        }
-    }
-    echo '</table>' . "<br>";
+    <table class="highlight">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Status</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+
+
+        <tbody>
+
+            <?php
+
+            while ($dados = mysqli_fetch_assoc($resultado)) {
+                if ($dados['situacao'] == "disponivel") {
+                    echo '<tr>';
+                    echo '<td>' . $dados['nome_projeto'] . '</td>';
+                    echo '<td>' . '<i class="material-icons left" style="color: #05CA5D;">brightness_1</i>' . $dados['situacao'] . '</td>';
+                    echo '<td> <a href="../../Crud/crud_encontro/listar.php?id_projeto=' . $dados['id_projeto'] . '" class="waves-effect waves-light  btn-flat"><i class="material-icons left">people</i>Encontros</a></a> </td>';
+                    echo '<td> <a href="../../Crud/crud_projeto/finalizar.php?id_projeto=' . $dados['id_projeto'] . '" class="waves-effect waves-light  btn-flat"><i class="material-icons left">flag</i>Finalizar</a></a></td>';
+                    echo '<td>  <a href="../../Crud/crud_projeto/excluir.php?id_projeto=' . $dados['id_projeto'] . '" class="waves-effect waves-light  btn-flat"><i class="material-icons">delete</i></a></td>';
+                    echo '</tr>';
+                } else {
+                    echo '<tr>';
+                    echo '<td>' . $dados['nome_projeto'] . '</td>';
+                    echo '<td>' . '<i class="material-icons left" style="color: #F20F10;">brightness_1</i>' . $dados['situacao'] . '</td>';
+                    echo '<td> <a href="../../certificado.php?id_projeto=' . $dados['id_projeto'] . '" class="waves-effect waves-light  btn-flat"><i class="material-icons left">book</i>Certificados</a></td>';
+                    echo '</tr>';
+                }
+            }
+            ?>
+        </tbody>
+
+
+    </table>
+
+<?php
+
 }
--- Login/professor/professor.php
+++ Login/professor/professor.php
@@ -1,36 +1,113 @@
 <?php
 
-if(!isset($_SESSION)){
-    session_start();
-}
-
+require_once "../../notificacao/funcaoNotificacao.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="pt-br">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
+    <title>Projeto</title>
+
+    <!-- CSS  -->
+    <link href="../../Style/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link href="../../Style/css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+
 </head>
+
 <body>
-    <h2><?php
-    echo "Bem vindo professor(a)" . $_SESSION['usuario'][0];
-    ?></h2>
-    <h1>Meus Projetos</h1>
-    <h3><a href="../../index.php">Sair</a></h3>
-    <h3><a href="../../Crud/crud_perfil/inicial.php">Meu Perfil</a></h3>
-    <h3>Nomes</h3>
-    <h3>Status</h3>
-    <h3>Ações</h3>
-    <a href="../../Crud/crud_projeto/formcad.php">Criar novo Projeto</a>
-    <hr></hr>
+    <div id="navbar" class="navbar-fixed scrollspy">
+        <nav class="white">
+            <div class="nav-wrapper container">
+                <div class="container">
+                    <a class="brand-logo"><img src="../../Style/images/logo.svg" style="height: 50px;"></a>
+                </div>
 
-    <?php
-    require_once('../../Crud/crud_projeto/listar.php')
-    ?>
+                <a href="" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+                <ul class="right hide-on-med-and-down">
 
-    
+                    <li><a href=""  > Projetos</a></li>
+                    <li><a href="../../Crud/crud_perfil/inicial.php"  >Meu Perfil</a></li>
+                    <li><a href="../../index.php"  >Sair</a></li>
+
+                </ul>
+                <ul class="right side-nav" id="mobile-demo">
+                    <li><a class="head-link" href="">Meus Projetos</a></li>
+                    <li><a class="head-link" href="../../Crud/crud_perfil/inicial.php">Meu Perfil</a></li>
+                    <li><a class="head-link" href="../../index.php">Sair</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+    <div class="container section scrollspy">
+        <div class="section">
+
+            <div class="row">
+
+                <div class="col s9">
+                    <h4>Meus Projetos</h4>
+                </div>
+                <div class="col s3">
+                    <div>
+                        <a href="../../Crud/crud_projeto/formcad.php" class="waves-effect waves-light black btn-large"><i class="material-icons left">add_box</i>criar novo projeto</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+
+             <?php
+
+             require_once "../../Crud/crud_projeto/listar.php";
+             ?>
+
+            </div>
+        </div>
+    </div>
+
+
+    <footer class="page-footer black lighten-1">
+
+        <div class="footer-copyright">
+            <div class="container">
+                <p>Copyright ©2025 Luiz</p>
+            </div>
+        </div>
+    </footer>
+
+
+    <!--  Scripts-->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="../../Style/js/materialize.js"></script>
+    <script src="../../Style/js/init.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.materialboxed').materialbox();
+            $('.button-collapse').sideNav();
+
+        });
+    </script>
+    <script>
+        $('.head-link').click(function(e) {
+            e.preventDefault();
+
+            var goto = $(this).attr('href');
+
+            $('html, body').animate({
+                scrollTop: $(goto).offset().top
+            }, 800);
+        });
+    </script>
+
 
 </body>
+
 </html>
