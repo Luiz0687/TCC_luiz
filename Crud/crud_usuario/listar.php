3dc904c2cdf5676f8de3b86f2172da05aa8be2b9
Luiz_Guilherme <luiz.2022310970@aluno.iffar.edu.br>
2025-01-27 17:37:07 -0300

    .

--- Crud/crud_usuario/listar.php
+++ Crud/crud_usuario/listar.php
@@ -5,23 +5,39 @@ require_once("../../conecta.php");
 $conexao = conectar();
 $id_projeto = $_GET['id_projeto'];
 
-// Seleciona os dados agrupando por usuário e somando a carga horária, considerando a presença/ausência na tabela de frequência
-$sql = "SELECT user.nome, 
-               SUM(CASE 
-                        WHEN freq.id_frequencia IS NOT NULL THEN en.CH 
-                        ELSE 0 
-                    END) AS total_CH
-        FROM projeto pro 
-        INNER JOIN encontro en ON en.fk_id_projeto = pro.id_projeto 
-        INNER JOIN usuario_projeto userpro ON userpro.fk_projeto_id_projeto = pro.id_projeto 
-        INNER JOIN usuario user ON user.id_usuario = userpro.fk_usuario_id_usuario 
-        LEFT JOIN frequencia freq ON freq.fk_id_encontro = en.id_encontro 
-                                   AND freq.fk_usuario_id_usuario = user.id_usuario
-        WHERE pro.id_projeto = $id_projeto 
-        GROUP BY user.id_usuario";
+// Verifica se o id_projeto está definido e é numérico
+if (!isset($id_projeto) || !is_numeric($id_projeto)) {
+    die("ID do projeto inválido.");
+}
 
-// Executa o Select
+// Corrige o SQL e adiciona alias para evitar ambiguidades
+$sql = "SELECT 
+            user.nome, 
+            userpro.id_inscricao, 
+            SUM(CASE WHEN freq.id_frequencia IS NOT NULL THEN en.CH ELSE 0 END) AS total_CH
+        FROM 
+            projeto pro
+        INNER JOIN 
+            encontro en ON en.fk_id_projeto = pro.id_projeto
+        INNER JOIN 
+            usuario_projeto userpro ON userpro.fk_projeto_id_projeto = pro.id_projeto
+        INNER JOIN 
+            usuario user ON user.id_usuario = userpro.fk_usuario_id_usuario
+        LEFT JOIN 
+            frequencia freq ON freq.fk_id_encontro = en.id_encontro
+            AND freq.fk_usuario_id_usuario = user.id_usuario
+        WHERE 
+            pro.id_projeto = $id_projeto
+        GROUP BY 
+            user.id_usuario;";
+
+// Executa o Select e verifica erros
 $resultado = mysqli_query($conexao, $sql);
+
+if (!$resultado) {
+    // Mostra o erro do MySQL
+    die("Erro na consulta SQL: " . mysqli_error($conexao));
+}
 
 // Lista os itens
 echo '<table border=1>
@@ -34,12 +50,11 @@ echo '<table border=1>
 // Exibe os dados
 while ($dados = mysqli_fetch_assoc($resultado)) {
     echo '<tr>';    
-    echo '<td>' . $dados['nome'] . '</td>';
-    echo '<td>' . $dados['total_CH'] . '</td>';
-    echo '<td><button><a href="../../certificado.php?verificador=' . $id_inscricao .  '">Emitir Certificado</a></button></td>';
+    echo '<td>' . ($dados['nome']) . '</td>';
+    echo '<td>' . ($dados['total_CH']) . '</td>';
+    echo '<td><button><a href="../../certificado.php?id_inscricao=' . ($dados['id_inscricao']) .  '&total_CH='.$dados['total_CH'].'">Emitir Certificado</a></button></td>';
     echo '</tr>';
 }
 
-echo '<hr>';
-echo '</table>' . "<br>";
+echo '</table>';
 ?>
